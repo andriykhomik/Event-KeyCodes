d8f9cb0e7d399b66a9401f3e6679172554f6bd54
andriykhomik <andriykhomik1992@gmail.com>
2021-11-10 23:40:45 +0200

    finished this project as in an example

--- main.js
+++ main.js
@@ -1,26 +1,18 @@
-const divKeys = document.querySelectorAll('.key');
+const insert = document.querySelector('.insert');
 
 addEventListener('keydown', (key)=>{
-
-    divKeys.forEach((divKey, i) => {
-        if (divKey.hasAttribute('id')){
-
-            if (divKey.hasAttribute('style')) divKey.attributes.removeNamedItem('style');
-
-            // this variant using ternary operator
-            (divKey.id === 'key') ? divKey.innerHTML = `${key.key === ' '? 'Space' : key.key}<small>event.key</small>` : '';
-            (divKey.id === 'keyCode') ? divKey.innerHTML = `${key.which}<small>event.keyCode</small>` : '';
-            (divKey.id === 'code') ? divKey.innerHTML = `${key.code}<small>event.code</small>` : '';
-
-            // this variant using "if" operator
-            /*if (divKey.id === 'key') divKey.innerHTML = `${key.key}<small>event.key</small>`
-            if (divKey.id === 'keyCode') divKey.innerHTML = `${key.keyCode}<small>event.keyCode</small>`;
-            if (divKey.id === 'code') divKey.innerHTML = `${key.code}<small>event.code</small>`;*/
-
-        } else {
-            divKeys[i].style.display = 'none';
-        }
-    })
+    insert.innerHTML = ` <div class="key" id="key">
+            ${key.key === ' '? 'Space' : key.key}
+            <small>event.key</small>
+        </div>
+        <div class="key" id="keyCode" >
+            ${key.keyCode}
+            <small>event.keyCode</small>
+        </div>
+        <div class="key" id="code" >
+            ${key.code}
+            <small>event.code</small>
+        </div>`
 })
 
 
